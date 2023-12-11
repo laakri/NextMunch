c241d50038f9de00aa533f3f1ed10f93c791ec30
Jelassi Saif <68915309+laakri@users.noreply.github.com>
2023-12-11 17:19:28 +0100

    Adding the Contact us /About Us page to the router
    amen tnajem just ta3mel pull w tbadel the pulled code with your existing code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { SignUpComponent } from './_Auth/sign-up/sign-up.component';
 import { LoginComponent } from './_Auth/login/login.component';
 import { HomepageComponent } from './_Client_Side/homepage/homepage.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomepageComponent },
+  { path: 'About', component: AboutUsComponent },
+  { path: 'Contact', component: ContactUsComponent },
   { path: 'SignUp', component: SignUpComponent },
   { path: 'Login', component: LoginComponent },
 ];
