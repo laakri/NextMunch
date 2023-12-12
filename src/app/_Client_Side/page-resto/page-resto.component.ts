01c9a10fb314640ff914fcc9421e8da0c629d2de
Jelassi Saif <68915309+laakri@users.noreply.github.com>
2023-12-12 13:35:08 +0100

    Add the resorant page
    Laakri

--- src/app/_Client_Side/page-resto/page-resto.component.ts
+++ src/app/_Client_Side/page-resto/page-resto.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-page-resto',
   templateUrl: './page-resto.component.html',
-  styleUrls: ['./page-resto.component.css']
+  styleUrls: ['./page-resto.component.css'],
 })
 export class PageRestoComponent {
-
+  events: any[] = [1, 2, 3];
 }
