081074b74870cc9b84d1ff633ead5183d27f083c
Jelassi Saif <68915309+laakri@users.noreply.github.com>
2023-12-14 12:47:53 +0100

    changing the restos componant

--- src/app/_Client_Side/restaurants/restaurants.component.ts
+++ src/app/_Client_Side/restaurants/restaurants.component.ts
@@ -3,14 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-restaurants',
   templateUrl: './restaurants.component.html',
-  styleUrls: ['./restaurants.component.css']
+  styleUrls: ['./restaurants.component.css'],
 })
 export class RestaurantsComponent {
-  ratingValue: number = 4;
   restaurants = Array(6).fill({
-    name: 'Chaneb',
-     location: 'Chaneb Eni 90, Tunisie',
-      description: 'Chaneb Eni 90, Tunisie  pas encore assez de notes sur ses plats, son service, son rapport qualité/prix ou son ambiance. Écrivezun des premiers avis !',
+    name: 'Weld El Hattab',
+    location: 'Chaneb Eni 90, Tunisie Eni 90, Tunisie Eni 90, Tunisie',
+    description:
+      'Chaneb Eni 90, Tunisie  pas encore assez de notes sur ses plats, son service, son rapport qualité/prix ou son ambiance. Écrivezun des premiers avis !',
     // Ajoutez d'autres restaurants si nécessaire
-  });  
+  });
 }
