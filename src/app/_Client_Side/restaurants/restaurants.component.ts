be1ff4820df886cc91b5043f66f9e948ef9a1c61
sheimazb <zbedichaima@gmail.com>
2023-12-14 15:49:15 +0100

    filter
    j'ai crée section filter resto dans page reastaurants :)

--- src/app/_Client_Side/restaurants/restaurants.component.ts
+++ src/app/_Client_Side/restaurants/restaurants.component.ts
@@ -6,6 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./restaurants.component.css'],
 })
 export class RestaurantsComponent {
+  selectedCategories: any[] = [];
+
+    categories: any[] = [
+        { name: 'Fast food', key: 'A' },
+        { name: 'Tunisienne ', key: 'M' },
+        { name: 'Vegeterienne', key: 'P' },
+        { name: 'Dessert', key: 'R' }
+    ];
+  // Dans votre composant Angular
+
   restaurants = Array(6).fill({
     name: 'Weld El Hattab',
     location: 'Chaneb Eni 90, Tunisie Eni 90, Tunisie Eni 90, Tunisie',
@@ -13,4 +23,7 @@ export class RestaurantsComponent {
       'Chaneb Eni 90, Tunisie  pas encore assez de notes sur ses plats, son service, son rapport qualité/prix ou son ambiance. Écrivezun des premiers avis !',
     // Ajoutez d'autres restaurants si nécessaire
   });
+  
+
+  
 }
