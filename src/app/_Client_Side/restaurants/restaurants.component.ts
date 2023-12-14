0807d2de90f7056c5cac64bc18b4fc4d9ec93ed5
sheimazb <zbedichaima@gmail.com>
2023-12-14 10:37:30 +0100

    page resataurants
    j'ai crée le frontend de page restaurants

--- src/app/_Client_Side/restaurants/restaurants.component.ts
+++ src/app/_Client_Side/restaurants/restaurants.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./restaurants.component.css']
 })
 export class RestaurantsComponent {
-
+  ratingValue: number = 4;
+  restaurants = Array(6).fill({
+    name: 'Chaneb',
+     location: 'Chaneb Eni 90, Tunisie',
+      description: 'Chaneb Eni 90, Tunisie  pas encore assez de notes sur ses plats, son service, son rapport qualité/prix ou son ambiance. Écrivezun des premiers avis !',
+    // Ajoutez d'autres restaurants si nécessaire
+  });  
 }
