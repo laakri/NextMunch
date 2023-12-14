2f3199d397e7236a42e8c5bf19b1d4266cc71ae7
Jelassi Saif <68915309+laakri@users.noreply.github.com>
2023-12-14 10:58:20 +0100

    adding the admin dashboard panel

---
+++ src/app/_Dashboard_Admin/list-restaurant/list-restaurant.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-restaurant',
+  templateUrl: './list-restaurant.component.html',
+  styleUrls: ['./list-restaurant.component.css']
+})
+export class ListRestaurantComponent {
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,12 @@ import { ContactUsComponent } from './_Client_Side/contact-us/contact-us.compone
 import { AuthComponent } from './_Auth/auth/auth.component';
 import { PageRestoComponent } from './_Client_Side/page-resto/page-resto.component';
 import { RestaurantsComponent } from './_Client_Side/restaurants/restaurants.component';
+/*************************   Admin Dashboard   *********************** */
+
+import { SidebarComponent } from './_Dashboard_Admin/sidebar/sidebar.component';
+import { ListAddCategoryComponent } from './_Dashboard_Admin/list-add-category/list-add-category.component';
+import { ListUserComponent } from './_Dashboard_Admin/list-user/list-user.component';
+import { ListRestaurantComponent } from './_Dashboard_Admin/list-restaurant/list-restaurant.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -26,6 +32,16 @@ const routes: Routes = [
       { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
       { path: 'login', component: LoginComponent },
       { path: 'signup', component: SignUpComponent },
+    ],
+  },
+  {
+    path: 'Dashboard',
+    component: SidebarComponent,
+    children: [
+      { path: '', redirectTo: '/Dashboard/ListAddCategory', pathMatch: 'full' },
+      { path: 'ListAddCategory', component: ListAddCategoryComponent },
+      { path: 'ListUser', component: ListUserComponent },
+      { path: 'ListRestaurant', component: ListRestaurantComponent },
     ],
   },
 ];
