53f518f36a3333a2ebe54aef48175587c2238ed3
Jelassi Saif <68915309+laakri@users.noreply.github.com>
2023-12-15 15:13:50 +0100

    Add the footer and font family

--- src/app/_Client_Side/restaurants/restaurants.component.ts
+++ src/app/_Client_Side/restaurants/restaurants.component.ts
@@ -8,22 +8,19 @@ import { Component } from '@angular/core';
 export class RestaurantsComponent {
   selectedCategories: any[] = [];
 
-    categories: any[] = [
-        { name: 'Fast food', key: 'A' },
-        { name: 'Tunisienne ', key: 'M' },
-        { name: 'Vegeterienne', key: 'P' },
-        { name: 'Dessert', key: 'R' }
-    ];
+  categories: any[] = [
+    { name: 'Fast food', key: 'A' },
+    { name: 'Tunisienne ', key: 'M' },
+    { name: 'Vegeterienne', key: 'P' },
+    { name: 'Dessert', key: 'R' },
+  ];
   // Dans votre composant Angular
 
-  restaurants = Array(6).fill({
+  restaurants = Array(8).fill({
     name: 'Weld El Hattab',
     location: 'Chaneb Eni 90, Tunisie Eni 90, Tunisie Eni 90, Tunisie',
     description:
       'Chaneb Eni 90, Tunisie  pas encore assez de notes sur ses plats, son service, son rapport qualité/prix ou son ambiance. Écrivezun des premiers avis !',
     // Ajoutez d'autres restaurants si nécessaire
   });
-  
-
-  
 }
