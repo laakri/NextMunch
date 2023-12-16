514541fc1441ce32dac32cdc497810c0a40d5c79
sheimazb <zbedichaima@gmail.com>
2023-12-16 10:28:31 +0100

    home page

--- src/app/_Client_Side/homepage/homepage.component.ts
+++ src/app/_Client_Side/homepage/homepage.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css']
+  styleUrls: ['./homepage.component.css'],
+  providers: [DialogService],
+
 })
 export class HomepageComponent {
+
+  items: MenuItem[] | undefined;
+  
 
 }
