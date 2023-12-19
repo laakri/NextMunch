36c73ad73da5e2fdc6ecec9ac00439992a410573
Jelassi Saif <68915309+laakri@users.noreply.github.com>
2023-12-19 14:51:31 +0100

    changing the event page

--- src/app/_Dashboard_Resto/add-event/add-event.component.ts
+++ src/app/_Dashboard_Resto/add-event/add-event.component.ts
@@ -17,37 +17,36 @@ export class AddEventComponent {
     'https://i.gyazo.com/aea585922385b1af33a6cc8ced47d193.png';
   imageArray: string[] = [
     'https://i.gyazo.com/aea585922385b1af33a6cc8ced47d193.png',
-    'https://i.gyazo.com/aea585922385b1af33a6cc8ced47d193.png',
-    'https://i.gyazo.com/aea585922385b1af33a6cc8ced47d193.png',
-    'https://i.gyazo.com/aea585922385b1af33a6cc8ced47d193.png',
+    'https://i.gyazo.com/187d2b398f8df7d36e63a9ea16f69ed4.png',
+    'https://i.gyazo.com/b5ae1a05eadab20cf4c12cf1ffa9b714.png',
     'https://i.gyazo.com/aea585922385b1af33a6cc8ced47d193.png',
     'https://i.gyazo.com/aea585922385b1af33a6cc8ced47d193.png',
   ];
   products: any[] = [
     {
-      nameP: 'Fake Product 1',
-      descriptionP: 'Description for Fake Product 1',
+      nameP: 'Product 1',
+      descriptionP: 'Description for Product 1',
       imgP: null,
       priceP: '10.99',
       selected: true,
     },
     {
-      nameP: 'Fake Product 2',
-      descriptionP: 'Description for Fake Product 2',
+      nameP: 'Product 2',
+      descriptionP: 'Description for Product 2',
       imgP: null,
       priceP: '15.99',
       selected: true,
     },
     {
-      nameP: 'Fake Product 3',
-      descriptionP: 'Description for Fake Product 3',
+      nameP: 'Product 3',
+      descriptionP: 'Description for Product 3',
       imgP: null,
       priceP: '20.99',
       selected: true,
     },
     {
-      nameP: 'Fake Product 4',
-      descriptionP: 'Description for Fake Product 4',
+      nameP: 'Product 4',
+      descriptionP: 'Description for Product 4',
       imgP: null,
       priceP: '8.99',
       selected: true,
