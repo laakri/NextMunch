d1ff7067f6ae2173b4e796b80b98cb1254c0c4a1
Jelassi Saif <68915309+laakri@users.noreply.github.com>
2023-12-20 10:12:21 +0100

    event page

--- src/app/_Dashboard_Resto/add-event/add-event.component.ts
+++ src/app/_Dashboard_Resto/add-event/add-event.component.ts
@@ -14,17 +14,18 @@ export class AddEventComponent {
   eventName: string | null = 'Weld El Hattab';
   // New properties for image selection
   selectedImage: string | null =
-    'https://i.gyazo.com/682dd298b6e0b689564f9353020d2b94.png';
+    'https://i.gyazo.com/b36542af0a7113f07edead7ad733439d.png';
+
   imageArray: string[] = [
-    'https://i.gyazo.com/682dd298b6e0b689564f9353020d2b94.png',
+    'https://i.gyazo.com/b36542af0a7113f07edead7ad733439d.png',
+    'https://i.gyazo.com/38a48cca08654c640e0c2312deb44497.png',
+    'https://i.gyazo.com/9ba965999576fd5295a8bc44a3219846.png',
     'https://i.gyazo.com/aea585922385b1af33a6cc8ced47d193.png',
     'https://i.gyazo.com/187d2b398f8df7d36e63a9ea16f69ed4.png',
     'https://i.gyazo.com/b5ae1a05eadab20cf4c12cf1ffa9b714.png',
     'https://i.gyazo.com/28731088c91275e416e91f13b1c0edbe.png',
     'https://i.gyazo.com/84a0ffa682c1e6a8486e49cf01e06359.png',
-    'https://i.gyazo.com/38a48cca08654c640e0c2312deb44497.png',
-    'https://i.gyazo.com/9ba965999576fd5295a8bc44a3219846.png',
-    'https://i.gyazo.com/b36542af0a7113f07edead7ad733439d.png',
+    'https://i.gyazo.com/682dd298b6e0b689564f9353020d2b94.png',
   ];
   products: any[] = [
     {
